b988aa78795b4beb414e565da1c4912619e4e122
phamthephuc <phamthephuc9649@gmail.com>
2020-08-05 10:45:09 +0700

    change test file

--- test.py
+++ test.py
@@ -4,34 +4,43 @@ from tqdm import tqdm
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
-import model.model as module_arch
+import model.crnn as module_arch
 from parse_config import ConfigParser
-
+import csv
+from utils import inf_loop, MetricTracker, strLabelConverter, averager, loadData, loadDataImage, countDifCharacter
 
 def main(config):
     logger = config.get_logger('test')
 
     # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
-    )
+    data_loader = config.init_obj('data_loader_test', module_data)
 
     # build model architecture
     model = config.init_obj('arch', module_arch)
     logger.info(model)
 
     # get function handles of loss and metrics
-    loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
+    criterion = config.init_obj("loss", module_loss)
+    # metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
     checkpoint = torch.load(config.resume)
     state_dict = checkpoint['state_dict']
+
+    opt = config["data_loader_test"]["args"]
+    batch_size = opt["batch_size"]
+    image = torch.FloatTensor(batchSize, 3, opt["imgH"], opt["imgH"])
+    text = torch.IntTensor(batchSize * 5)
+    length = torch.IntTensor(batchSize)
+
+    if torch.cuda.is_available():
+        image = image.cuda()
+        criterion = criterion.cuda()
+    image = Variable(image)
+    text = Variable(text)
+    length = Variable(length)
+
+
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
     model.load_state_dict(state_dict)
@@ -41,31 +50,64 @@ def main(config):
     model = model.to(device)
     model.eval()
 
-    total_loss = 0.0
-    total_metrics = torch.zeros(len(metric_fns))
-
+    # total_loss = 0.0
+    # total_metrics = torch.zeros(len(metric_fns))
+    n_correct = 0
+    sum_character_error = 0
+    max_iter = len(data_loader)
+    loss_avg = averager()
     with torch.no_grad():
         for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
 
-            #
-            # save sample images, or do something with output here
-            #
+            batch_size = data.size(0)
+            # data, target = data.to(self.device), target.to(self.device)
 
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
+            text, length = self.converter.encode(target)
 
-    n_samples = len(data_loader.sampler)
-    log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
-    logger.info(log)
+            loadData(image, data)
+            output = self.model(image)
+            t, l = self.converter.encode(target)
+            loadData(text, t)
+            loadData(length, l)
+
+            output_size = Variable(torch.IntTensor([output.size(0)] * batch_size))
+            loss = self.criterion(output, text, output_size, length) / batch_size
+            loss_avg.add(loss)
+
+            # self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
+            # self.valid_metrics.update('loss', loss_avg.val())
+
+            _, output = output.max(2)
+            # preds = preds.squeeze(2)
+            output = output.transpose(1, 0).contiguous().view(-1)
+            sim_preds = self.converter.decode(output.data, output_size.data, raw=False)
+            with open('result.csv', 'w', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=' ',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+                for pred, tart in zip(sim_preds, target):
+                    tart = tart.lower
+                    if pred == tart:
+                        n_correct += 1
+                        sum_character_error += countDifCharacter(pred, tart)
+                        spamwriter.writerow(tart, pred, "OK")
+                    else:
+                        spamwriter.writerow(tart, pred, "NG")
+
+
+
+            # for met in self.metric_ftns:
+            #     self.valid_metrics.update(met.__name__, met(sim_preds, target))
+            # self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+
+        raw_preds = self.converter.decode(output.data, output_size.data, raw=True)[1000]
+        for raw_pred, pred, gt in zip(raw_preds, sim_preds, target):
+            print('%-20s => %-20s, gt: %-20s' % (raw_pred, pred, gt))
+
+        accuracy = n_correct / float(max_iter * batch_size)
+        character_error_rate = sum_character_error / float(max_iter * batch_size)
+        print('Test loss: %f, accuray: %f, character error rate: %f' % (loss_avg.val(), accuracy, character_error_rate))
+
 
 
 if __name__ == '__main__':
