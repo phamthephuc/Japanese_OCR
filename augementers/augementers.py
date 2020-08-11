bb0c650c0f1bc6025a8ad18fad55385ffad2fca9
phamthephuc <phamthephuc9649@gmail.com>
2020-08-11 11:15:29 +0700

    add augementers

--- augementers/augementers.py
+++ augementers/augementers.py
@@ -3,12 +3,20 @@
 from PIL import Image, ImageOps
 from skimage.util import random_noise
 from skimage.filters import gaussian
+import numpy as np
 
 def invert(image):
     return ImageOps.invert(image)
 
-def noise(image):
-    return random_noise(image, var=0.2**2)
+def noisy(image):
+    row,col,ch= image.shape
+    mean = 0
+    var = 0.1
+    sigma = var**0.5
+    gauss = np.random.normal(mean,sigma,(row,col,ch))
+    gauss = gauss.reshape(row,col,ch)
+    noisy = image + gauss
+    return noisy
 
 def blur(image):
     return gaussian(image,sigma=1,multichannel=True)
