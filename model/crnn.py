7d46c2738809cfcbd966c238326c7fd8b17cb2c7
phamthephuc <phamthephuc9649@gmail.com>
2020-09-08 13:33:48 +0900

    change to train last layers

--- model/crnn.py
+++ model/crnn.py
@@ -1,6 +1,6 @@
 from base.base_model import BaseModel
 import torch.nn as nn
-
+from model.feature_extraction import *
 
 class BidirectionalLSTM(BaseModel):
 
@@ -20,50 +20,13 @@ class BidirectionalLSTM(BaseModel):
 
         return output
 
-
 class CRNN(BaseModel):
 
     def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
         super(CRNN, self).__init__()
         assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
 
-        ks = [3, 3, 3, 3, 3, 3, 3, 3]
-        ps = [1, 1, 1, 1, 1, 1, 1, 0]
-        ss = [1, 1, 1, 1, 1, 1, 1, 1]
-        nm = [32, 64, 128, 256, 256, 512, 512, 512]
-
-        cnn = nn.Sequential()
-
-        def convRelu(i, batchNormalization=False):
-            nIn = nc if i == 0 else nm[i - 1]
-            nOut = nm[i]
-            cnn.add_module('conv{0}'.format(i),
-                           nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
-            if batchNormalization:
-                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
-            if leakyRelu:
-                cnn.add_module('relu{0}'.format(i),
-                               nn.LeakyReLU(0.2, inplace=True))
-            else:
-                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
-
-        convRelu(0)
-        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 32x24x64
-        convRelu(1)
-        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 64x12x32
-        convRelu(2)
-        cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d(2, 2))  # 128x6x16
-        convRelu(3, True)
-        convRelu(4)
-        cnn.add_module('pooling{0}'.format(3),
-                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x3x 8
-        convRelu(5, True)
-        convRelu(6)
-        # cnn.add_module('pooling{0}'.format(4),
-        #                nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x 2 x16
-        convRelu(7, True)  # 512x1x16
-
-        self.cnn = cnn
+        self.cnn = ResNet_FeatureExtractor(imgH, 512)
         self.rnn = nn.Sequential(
             BidirectionalLSTM(512, nh, nh),
             BidirectionalLSTM(nh, nh, nclass))
@@ -81,3 +44,64 @@ class CRNN(BaseModel):
         output = self.rnn(conv)
 
         return output
+
+# class CRNN(BaseModel):
+#
+#     def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+#         super(CRNN, self).__init__()
+#         assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+#
+#         ks = [3, 3, 3, 3, 3, 3, 3, 3]
+#         ps = [1, 1, 1, 1, 1, 1, 1, 0]
+#         ss = [1, 1, 1, 1, 1, 1, 1, 1]
+#         nm = [32, 64, 128, 256, 256, 512, 512, 512]
+#
+#         cnn = nn.Sequential()
+#
+#         def convRelu(i, batchNormalization=False):
+#             nIn = nc if i == 0 else nm[i - 1]
+#             nOut = nm[i]
+#             cnn.add_module('conv{0}'.format(i),
+#                            nn.Conv2d(nIn, nOut, ks[i], ss[i], ps[i]))
+#             if batchNormalization:
+#                 cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+#             if leakyRelu:
+#                 cnn.add_module('relu{0}'.format(i),
+#                                nn.LeakyReLU(0.2, inplace=True))
+#             else:
+#                 cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+#
+#         convRelu(0)
+#         cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 32x24x64
+#         convRelu(1)
+#         cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 64x12x32
+#         convRelu(2)
+#         cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d(2, 2))  # 128x6x16
+#         convRelu(3, True)
+#         convRelu(4)
+#         cnn.add_module('pooling{0}'.format(3),
+#                        nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x3x 8
+#         convRelu(5, True)
+#         convRelu(6)
+#         # cnn.add_module('pooling{0}'.format(4),
+#         #                nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x 2 x16
+#         convRelu(7, True)  # 512x1x16
+#
+#         self.cnn = cnn
+#         self.rnn = nn.Sequential(
+#             BidirectionalLSTM(512, nh, nh),
+#             BidirectionalLSTM(nh, nh, nclass))
+#
+#     def forward(self, input):
+#         # conv features
+#         conv = self.cnn(input)
+#         b, c, h, w = conv.size()
+#         # print(b, c, h, w)
+#         assert h == 1, "the height of conv must be 1"
+#         conv = conv.squeeze(2)
+#         conv = conv.permute(2, 0, 1)  # [w, b, c]
+#
+#         # rnn features
+#         output = self.rnn(conv)
+#
+#         return output
--- model/model.py
+++ model/model.py
@@ -1,22 +1,113 @@
+"""
+Copyright (c) 2019-present NAVER Corp.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import torch.nn as nn
-import torch.nn.functional as F
-from base import BaseModel
+from base.base_model import BaseModel
+
+from model.transformation import TPS_SpatialTransformerNetwork
+from model.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from model.sequence_modeling import BidirectionalLSTM
+from model.prediction import Attention
+
+# class CRNN2(BaseModel):
+#
+#     def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+#         self.FeatureExtraction = ResNet_FeatureExtractor(1, 256)
+#         self.FeatureExtraction_output = 512  # int(imgH/16-1) * 512
+#         self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
+#         self.SequenceModeling = nn.Sequential(
+#             BidirectionalLSTM(512, nh, nh),
+#             BidirectionalLSTM(nh, nh, nclass))
+#         self.Prediction = Attention(nclass, nh, nclass)
+#
+#     def forward(self, input, text, is_train=True):
+#         """ Feature extraction stage """
+#         visual_feature = self.FeatureExtraction(input)
+#         visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
+#         visual_feature = visual_feature.squeeze(3)
+#
+#         contextual_feature = self.SequenceModeling(visual_feature)
+#         prediction = self.Prediction(contextual_feature.contiguous(), text, is_train,
+#                                      batch_max_length=self.opt.batch_max_length)
+#
+#         return prediction
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+class Model(nn.Module):
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+    def __init__(self, opt):
+        super(Model, self).__init__()
+        self.opt = opt
+        self.stages = {'Trans': opt.Transformation, 'Feat': opt.FeatureExtraction,
+                       'Seq': opt.SequenceModeling, 'Pred': opt.Prediction}
+
+        """ Transformation """
+        if opt.Transformation == 'TPS':
+            self.Transformation = TPS_SpatialTransformerNetwork(
+                F=opt.num_fiducial, I_size=(opt.imgH, opt.imgW), I_r_size=(opt.imgH, opt.imgW), I_channel_num=opt.input_channel)
+        else:
+            print('No Transformation module specified')
+
+        """ FeatureExtraction """
+        if opt.FeatureExtraction == 'VGG':
+            self.FeatureExtraction = VGG_FeatureExtractor(opt.input_channel, opt.output_channel)
+        elif opt.FeatureExtraction == 'RCNN':
+            self.FeatureExtraction = RCNN_FeatureExtractor(opt.input_channel, opt.output_channel)
+        elif opt.FeatureExtraction == 'ResNet':
+            self.FeatureExtraction = ResNet_FeatureExtractor(opt.input_channel, opt.output_channel)
+        else:
+            raise Exception('No FeatureExtraction module specified')
+        self.FeatureExtraction_output = opt.output_channel  # int(imgH/16-1) * 512
+        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
+
+        """ Sequence modeling"""
+        if opt.SequenceModeling == 'BiLSTM':
+            self.SequenceModeling = nn.Sequential(
+                BidirectionalLSTM(self.FeatureExtraction_output, opt.hidden_size, opt.hidden_size),
+                BidirectionalLSTM(opt.hidden_size, opt.hidden_size, opt.hidden_size))
+            self.SequenceModeling_output = opt.hidden_size
+        else:
+            print('No SequenceModeling module specified')
+            self.SequenceModeling_output = self.FeatureExtraction_output
+
+        """ Prediction """
+        if opt.Prediction == 'CTC':
+            self.Prediction = nn.Linear(self.SequenceModeling_output, opt.num_class)
+        elif opt.Prediction == 'Attn':
+            self.Prediction = Attention(self.SequenceModeling_output, opt.hidden_size, opt.num_class)
+        else:
+            raise Exception('Prediction is neither CTC or Attn')
+
+    def forward(self, input, text, is_train=True):
+        """ Transformation stage """
+        if not self.stages['Trans'] == "None":
+            input = self.Transformation(input)
+
+        """ Feature extraction stage """
+        visual_feature = self.FeatureExtraction(input)
+        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
+        visual_feature = visual_feature.squeeze(3)
+
+        """ Sequence modeling stage """
+        if self.stages['Seq'] == 'BiLSTM':
+            contextual_feature = self.SequenceModeling(visual_feature)
+        else:
+            contextual_feature = visual_feature  # for convenience. this is NOT contextually modeled by BiLSTM
+
+        """ Prediction stage """
+        if self.stages['Pred'] == 'CTC':
+            prediction = self.Prediction(contextual_feature.contiguous())
+        else:
+            prediction = self.Prediction(contextual_feature.contiguous(), text, is_train, batch_max_length=self.opt.batch_max_length)
+
+        return prediction
