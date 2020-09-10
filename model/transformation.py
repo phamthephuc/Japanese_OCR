56bdbedb3fa8930416a058f06666dde7ec7b2f28
phamthephuc <phamthephuc9649@gmail.com>
2020-09-11 00:44:47 +0900

    change to TRLA

--- model/transformation.py
+++ model/transformation.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1')
 
 
 class TPS_SpatialTransformerNetwork(nn.Module):
